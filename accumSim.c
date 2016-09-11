f69573c2d7ee0d3e42d5dd1c99230e36a6f6bf54
B-A-S-T <itfairhope@aol.com>
2016-09-11 17:54:17 -0500

    Comments

--- accumSim.c
+++ accumSim.c
@@ -1,57 +1,45 @@
-/*Using 32 bit addressing we will be able to access 4GB of data. 
+/*
+  Group 1
+  Created September 2016 by Ian Thomas with the help of Lanxin Ma.
+
+  Using 32 bit addressing we will be able to access 4GB of data.
   4294967296 decimal and 0x100000000 hex
   With addresses, words, and instructions all being 32 bits each
-  we will have a total of 134217728 memory locations simulated using arrays.(index's).
-  
+
   The text segment will start at address 0x50000000
   The data segment will start at address 0x40000000
-  The stack segment will start at address 0x30000000
   The kernal segment will start at address 0x60000000
-
-    Note not all address space is being use, but that is fine for the purpose of the assignment. Each segment is given 8388608 words.
-
-  We can access each word in the segement when given its address by subtracting that address
-    from the bottom most address and then right shifting that value 2, to simulate dividing
-    by 2^2. segment[(currentAddress - botAdress) >> 2].
-
-                    example: text_segment[(0x50000008 - 0x50000000) >> 2]
-                        =   text_segement[(8) >> 2] = text_segment[2];
-                        Address is converted so we can access it with our array.
 */
 
-/* Typedefs and Defines*/
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdint.h> // int32_t
+#include <stdio.h> // pritntf, getline, fopen
+#include <stdlib.h> // strtol
+#include <string.h> // strtok, strcmp, strcspn
 
+/* Typedefs and Defines*/
 typedef int32_t mem_addr;
 typedef int32_t mem_word;
 typedef int32_t instruction;
 
 #define MAX_SEG_SIZE 33554428
+#define NUM_STRINGS 10
+#define STRING_SIZE 100
 #define TEXT_START ((mem_word) 0x50000000)
 #define DATA_START ((mem_word) 0x40000000)
 #define KERNAL_START ((mem_word) 0x60000000)
-
-#define NUM_OF_TOKENS 200
-/* We subtract 1 mem_word(32 bits) from the address to get the top */
-
 #define DATA_TOP (TEXT_START -1)
 #define TEXT_TOP (KERNAL_TOP -1)
 #define KERNAL_TOP (((mem_word) (0x70000000)) -1)
 
 /*Global Variables:  */
-
 instruction* text_segment;
 mem_word *data_segment;
-mem_word *stack_segment;
 mem_word *kernal_segment;
 mem_addr data_limit;
-mem_word *source_tokens[NUM_OF_TOKENS];
+int last_string = 0;
+char strings[NUM_STRINGS][STRING_SIZE];
 
 /* Prototypes */
-
 void make_memory();
 void parse_source_code(char *filename);
 void load_data(char* token);
@@ -64,8 +52,9 @@ void write_mem(mem_addr address, mem_word value);
 void add(mem_word* accum, mem_addr operand);
 void mult(mem_word* accum, mem_addr operand);
 
+/*Main entry into simulator, one argument should be passed, the filename.*/
 int main(int argc, char *argv[]){
-    
+
     make_memory();
     parse_source_code(argv[1]);
     mem_word accumulator;
@@ -74,15 +63,16 @@ int main(int argc, char *argv[]){
     mem_word to_load = 0;
     mem_addr to_store = 0;
     mem_addr to_add = 0;
-    mem_addr to_mult = 0; 
+    mem_addr to_mult = 0;
     mem_addr pc = TEXT_START;
+    mem_addr to_print;
 
     while(usermode){
         instr = read_inst(pc);
         pc += 1;
         switch(instr){
             case 0:
-                to_load = (mem_word) read_inst(pc); 
+                to_load = (mem_word) read_inst(pc);
                 accumulator = read_mem(to_load);
                 pc += 1;
                 break;
@@ -103,21 +93,28 @@ int main(int argc, char *argv[]){
                 break;
             case 4:
                 usermode = 0;
-                printf("Value in Accumulator:  %d\n\n", accumulator);
+                printf("%d\n", accumulator);
+                break;
+            case 5:
+                to_print = (mem_addr) read_inst(pc);
+                if(to_print >= last_string || to_print < 0){
+                    printf("Failed to print that string: Wrong Index");
+                }
+                printf("%s", strings[to_print]);
+                pc += 1;
                 break;
         }
     }
     free(data_segment);
-    free(stack_segment);
     free(kernal_segment);
     free(text_segment);
     return 0;
 }
 
-/* Each segment will have 33,554,431 bytes, divide this by
-
-    4 and we get 8,388,607 indexes in each array*/
-
+/*Description: Creates memory for the memory segments.
+  Params: NONE
+  Returns: void
+*/
 void make_memory(){
 
     data_segment = malloc(MAX_SEG_SIZE);
@@ -127,11 +124,14 @@ void make_memory(){
     if(kernal_segment == NULL)
         exit(1);
     text_segment = malloc(MAX_SEG_SIZE);
-    if(text_segment == NULL) 
+    if(text_segment == NULL)
         exit(1);
 
 }
-
+/*Description: Loads the source code elements into simulated memory.
+  Params: *filename -name of source code file
+  Returns: void
+*/
 void parse_source_code(char *filename){
     FILE *fp;
     short data_load = 0;
@@ -149,14 +149,16 @@ void parse_source_code(char *filename){
     while((read = getline(&line, &length, fp)) != -1){
             if(strcmp(line, "\n") != 0){
             line[strcspn(line, "\n")] = 0;
+            //In data section of parse
                 if(strcmp(line, ".data") == 0){
                      data_load = 1;
                      continue;
                 }
+            //Exit data section of parse
                 if(strcmp(line, ".text") == 0){
                      data_load = 0;
                      continue;
-}               
+}
                 if(data_load == 1){
                     load_data(line);
                 }
@@ -168,21 +170,35 @@ void parse_source_code(char *filename){
 
 }
 
-load_data(char* token){
+/*Description: Loads data elements from source into simulated data segment.
+  Params: *token - essentially the line that was read
+  Returns: void
+*/
+void load_data(char* token){
     char *address = NULL;
     char *value = NULL;
-    mem_addr addr = 0; 
+    mem_addr addr = 0;
     mem_word val = 0;
-    
-    address = strtok(token, " \t");
-    value = strtok(NULL, " \t");
 
-    addr = (mem_addr) strtol(address, (char **)NULL, 16);
-    val = (mem_word) strtol(value, (char **)NULL, 16);
-    
-    write_mem(addr, val);
+    address = strtok(token, " \t");
+    //Checking for a data of type string
+    if(strcmp(address, ".asciiz") == 0){
+       address = strtok(NULL, "\"") ;
+        strcpy(strings[last_string], address);
+        last_string += 1;
+    }
+    else{
+      value = strtok(NULL, " \t");
+      addr = (mem_addr) strtol(address, (char **)NULL, 16);
+      val = (mem_word) strtol(value, (char **)NULL, 16);
+      write_mem(addr, val);
+    }
 }
 
+/*Description: Loads source code instructions into simulated text segment.
+  Params: *index - program counter incremented for next instruction entry
+  Returns: void
+*/
 void load_text(char* token, int *index){
     char *instr = strtok(token, " \t");
     instruction op_code = 0;
@@ -194,9 +210,13 @@ void load_text(char* token, int *index){
         *index += 1;
         instr = strtok(NULL, " \t");
     }
-    
+
 }
 
+/*Description: Gets the op code for an instruction for easier switch statement.
+  Params: *instr - instruction that was parsed from source file
+  Returns: instruction - opcode number or address for an instruction
+*/
 instruction get_opCode(char *instr){
     if(strcmp(instr, "LOAD") == 0) return 0;
     else if (strcmp(instr, "STO") == 0) return 1;
@@ -209,6 +229,10 @@ instruction get_opCode(char *instr){
 
 /* For each of the reads and writes we first check to see if the address
     is less than its address top boundary and then the bottom.*/
+/*Description: Read memory from the data_segment.
+          Params: address - address to read from
+          Returns: mem_word - returns the contents
+*/
 mem_word read_mem(mem_addr address){
     if((address <  DATA_TOP) && (address >= DATA_START))
         return data_segment[(address - DATA_START)];
@@ -218,6 +242,10 @@ mem_word read_mem(mem_addr address){
     }
 }
 
+/*Description: Read memory from the text_segment.
+      Params: address - address to read from
+      Returns: insrtuction - returns the contents
+*/
 instruction read_inst(mem_addr address){
     if((address <  TEXT_TOP) && (address >= TEXT_START)){
         return text_segment[(address - TEXT_START)];
@@ -228,6 +256,10 @@ instruction read_inst(mem_addr address){
     }
 }
 
+/*Description: Writes memory to the text_segment.
+      Params: address - address to read from, instr- instruction to write
+      Returns: void
+*/
 void write_instr(mem_addr address, instruction instr){
 
     if((address <  TEXT_TOP) && (address >= TEXT_START))
@@ -238,7 +270,10 @@ void write_instr(mem_addr address, instruction instr){
     }
 }
 
-
+/*Description: Writes memory to the data_segment.
+      Params: address - address to read from, instr- data to write
+      Returns: void
+*/
 void write_mem(mem_addr address, mem_word value){
     if((address < DATA_TOP) && (address >= DATA_START))
         data_segment[(address - DATA_START)] = value;
@@ -248,28 +283,23 @@ void write_mem(mem_addr address, mem_word value){
     }
 }
 
+/*Description: Adds value that is in the accumulator and a value from
+                a memory location.
+      Params: *accum- pointer to the accumulator, addr of the operand to add
+      Returns: void
+*/
 void add(mem_word* accum, mem_addr operand){
     mem_word quotient = read_mem(operand);
     *accum = quotient + *accum;
 }
 
+/*Description: Multiplies value that is in the accumulator and a value from
+                a memory location.
+      Params: *accum- pointer to the accumulator, addr of the operand to multiply
+      Returns: void
+*/
 void mult(mem_word* accum, mem_addr operand){
 
     mem_word product = read_mem(operand);
     *accum = product * (*accum);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- stackSim.c
+++ stackSim.c
@@ -1,65 +1,49 @@
-/*Using 32 bit addressing we will be able to access 4GB of data. 
-  4294967296 decimal and 0x100000000 hex
-  With addresses, words, and instructions all being 32 bits each
-  we will have a total of 134217728 memory locations simulated using arrays.(index's).
-  
+/*
+  Group 1
+  Created September 2016 by Ian Thomas with the help of Lanxin Ma.
+
+  Using 32 bit addressing we will be able to access
+  4294967296 decimal and 0x100000000 hex different addresses.
+  With addresses, words, and instructions all being 32 bits each.
+
   The text segment will start at address 0x50000000
   The data segment will start at address 0x40000000
   The stack segment will start at address 0x30000000
   The kernal segment will start at address 0x60000000
-
-    Note not all address space is being use, but that is fine for the purpose of the assignment. Each segment is given 8388608 words.
-
-  We can access each word in the segement when given its address by subtracting that address
-    from the bottom most address and then right shifting that value 2, to simulate dividing
-    by 2^2. segment[(currentAddress - botAdress) >> 2].
-
-                    example: text_segment[(0x50000008 - 0x50000000) >> 2]
-                        =   text_segement[(8) >> 2] = text_segment[2];
-                        Address is converted so we can access it with our array.
 */
 
-
+#include <stdint.h> // int32_t
+#include <stdio.h> // pritntf, getline, fopen
+#include <stdlib.h> // strtol
+#include <string.h> // strtok, strcmp, strcspn
 
 /* Typedefs and Defines*/
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 typedef int32_t mem_addr;
 typedef int32_t mem_word;
 typedef int32_t instruction;
 
-#define MAX_SEG_SIZE 33554428
+#define MAX_SEG_SIZE 8888888
 #define NUM_STRINGS 10
 #define STRING_SIZE 100
 #define TEXT_START ((mem_word) 0x50000000)
 #define DATA_START ((mem_word) 0x40000000)
-#define STACK_BOTTOM  ((mem_addr) 0x70000000) 
+#define STACK_BOTTOM  ((mem_addr) 0x70000000)
 #define KERNAL_START ((mem_word) 0x60000000)
-
-#define NUM_OF_TOKENS 200
-/* We subtract 1 mem_word(32 bits) from the address to get the top */
-
 #define DATA_TOP (TEXT_START -1)
 #define TEXT_TOP (KERNAL_TOP -1)
 #define KERNAL_TOP (((mem_word) (0x70000000)) -1)
 
 /*Global Variables:  */
-
 instruction* text_segment;
 mem_word *data_segment;
 mem_word *stack_segment;
 mem_word *kernal_segment;
 mem_addr stack_top = STACK_BOTTOM;
 mem_addr data_limit;
-mem_word *source_tokens[NUM_OF_TOKENS];
 int last_string = 0;
 char strings[NUM_STRINGS][STRING_SIZE];
 
 /* Prototypes */
-
 void make_memory();
 void parse_source_code(char *filename);
 void load_data(char* token);
@@ -74,8 +58,9 @@ void pop(mem_addr address);
 void add();
 void mult();
 
+/*Main entry into simulator, one argument should be passed, the filename.*/
 int main(int argc, char *argv[]){
-    
+
     make_memory();
     parse_source_code(argv[1]);
     instruction instr;
@@ -129,12 +114,11 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
-/* Each segment will have 33,554,431 bytes, divide this by
-
-    4 and we get 8,388,607 indexes in each array*/
-
+/*Description: Creates memory for the memory segments.
+  Params: NONE
+  Returns: void
+*/
 void make_memory(){
-
     data_segment = malloc(MAX_SEG_SIZE);
     if(data_segment == NULL)
         exit(1);
@@ -145,11 +129,15 @@ void make_memory(){
     if(kernal_segment == NULL)
         exit(1);
     text_segment = malloc(MAX_SEG_SIZE);
-    if(text_segment == NULL) 
+    if(text_segment == NULL)
         exit(1);
 
 }
 
+/*Description: Loads the source code elements into simulated memory.
+  Params: *filename -name of source code file
+  Returns: void
+*/
 void parse_source_code(char *filename){
     FILE *fp;
     short data_load = 0;
@@ -167,14 +155,16 @@ void parse_source_code(char *filename){
     while((read = getline(&line, &length, fp)) != -1){
             if(strcmp(line, "\n") != 0){
             line[strcspn(line, "\n")] = 0;
+            //In data section of parse
                 if(strcmp(line, ".data") == 0){
                      data_load = 1;
                      continue;
                 }
+            //Exit data section of parse
                 if(strcmp(line, ".text") == 0){
                      data_load = 0;
                      continue;
-}               
+}
                 if(data_load == 1){
                     load_data(line);
                 }
@@ -186,28 +176,35 @@ void parse_source_code(char *filename){
 
 }
 
-load_data(char* token){
+/*Description: Loads data elements from source into simulated data segment.
+  Params: *token - essentially the line that was read
+  Returns: void
+*/
+void load_data(char* token){
     char *address = NULL;
     char *value = NULL;
-    mem_addr addr = 0; 
+    mem_addr addr = 0;
     mem_word val = 0;
-    
+
     address = strtok(token, " \t");
+    //Checking for a data of type string, if so add to strings
     if(strcmp(address, ".asciiz") == 0){
        address = strtok(NULL, "\"") ;
         strcpy(strings[last_string], address);
         last_string += 1;
     }
     else{
-    value = strtok(NULL, " \t");
-
-    addr = (mem_addr) strtol(address, (char **)NULL, 16);
-    val = (mem_word) strtol(value, (char **)NULL, 16);
-    
-    write_mem(addr, val);
+      value = strtok(NULL, " \t");
+      addr = (mem_addr) strtol(address, (char **)NULL, 16);
+      val = (mem_word) strtol(value, (char **)NULL, 16);
+      write_mem(addr, val);
     }
 }
 
+/*Description: Loads source code instructions into simulated text segment.
+  Params: *index - program counter incremented for next instruction entry
+  Returns: void
+*/
 void load_text(char* token, int *index){
     char *instr = strtok(token, " \t");
     instruction op_code = 0;
@@ -219,9 +216,13 @@ void load_text(char* token, int *index){
         *index += 1;
         instr = strtok(NULL, " \t");
     }
-    
+
 }
 
+/*Description: Gets the op code for an instruction for easier switch statement.
+  Params: *instr - instruction that was parsed from source file
+  Returns: instruction - opcode number or address for an instruction
+*/
 instruction get_opCode(char *instr){
     if(strcmp(instr, "PUSH") == 0) return 0;
     else if (strcmp(instr, "POP") == 0) return 1;
@@ -234,6 +235,10 @@ instruction get_opCode(char *instr){
 
 /* For each of the reads and writes we first check to see if the address
     is less than its address top boundary and then the bottom.*/
+/*Description: Read memory from the data_segment.
+      Params: address - address to read from
+      Returns: mem_word - returns the contents
+*/
 mem_word read_mem(mem_addr address){
     if((address <  DATA_TOP) && (address >= DATA_START))
         return data_segment[(address - DATA_START)];
@@ -243,6 +248,10 @@ mem_word read_mem(mem_addr address){
     }
 }
 
+/*Description: Read memory from the text_segment.
+      Params: address - address to read from
+      Returns: insrtuction - returns the contents
+*/
 instruction read_inst(mem_addr address){
     if((address <  TEXT_TOP) && (address >= TEXT_START)){
         return text_segment[(address - TEXT_START)];
@@ -253,6 +262,10 @@ instruction read_inst(mem_addr address){
     }
 }
 
+/*Description: Writes memory to the text_segment.
+      Params: address - address to read from, instr- instruction to write
+      Returns: void
+*/
 void write_instr(mem_addr address, instruction instr){
 
     if((address <  TEXT_TOP) && (address >= TEXT_START))
@@ -263,7 +276,10 @@ void write_instr(mem_addr address, instruction instr){
     }
 }
 
-
+/*Description: Writes memory to the data_segment.
+      Params: address - address to read from, instr- data to write
+      Returns: void
+*/
 void write_mem(mem_addr address, mem_word value){
     if((address < DATA_TOP) && (address >= DATA_START))
         data_segment[(address - DATA_START)] = value;
@@ -273,12 +289,20 @@ void write_mem(mem_addr address, mem_word value){
     }
 }
 
+/*Description: Push a value on the stack at a memory address.
+      Params: address - address of value to push
+      Returns: void
+*/
 void push(mem_addr address){
         mem_word value = read_mem(address);
         stack_top += 1;
         stack_segment[(stack_top - STACK_BOTTOM)] = value;
 }
 
+/*Description: Pop the top value of the stack into an address.
+      Params: address - address of value to pop into
+      Returns: void
+*/
 void pop(mem_addr address){
     if(address == -1){
         stack_top -= 1;
@@ -293,22 +317,28 @@ void pop(mem_addr address){
     write_mem(address, value);
 }
 
+/*Description: Adds top two values on the stack, pops them off, then pushes the
+                result back on.
+      Params: NONE
+      Returns: void
+*/
 void add(){
-    mem_word added_value = stack_segment[(stack_top - STACK_BOTTOM)] 
+    mem_word added_value = stack_segment[(stack_top - STACK_BOTTOM)]
             + stack_segment[(stack_top - STACK_BOTTOM - 1)];
     pop(-1);
     stack_segment[stack_top - STACK_BOTTOM] = added_value;
 
 }
 
+/*Description: Multiplies top two values on the stack, pops them off, then pushes the
+                result back on.
+      Params: NONE
+      Returns: void
+*/
 void mult(){
-    mem_word added_value = stack_segment[(stack_top - STACK_BOTTOM)] 
+    mem_word added_value = stack_segment[(stack_top - STACK_BOTTOM)]
             * stack_segment[(stack_top - STACK_BOTTOM - 1)];
     pop(-1);
     stack_segment[stack_top - STACK_BOTTOM] = added_value;
 
 }
-
-
-
-
